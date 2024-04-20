729970b38bcdcc52c234c6cd0f2fa2a0ff5c4a59
Maulana Adam Sahid <masadamsahid@gmail.com>
2024-04-21 04:42:28 +0700

    send message api

---
+++ src/pages/api/socket/messages.ts
@@ -0,0 +1,62 @@
+import { NextApiRequest } from "next";
+
+import { NextApiResponseServerIo } from "@/types";
+import { currentProfilePages } from "@/lib/current-profile-pages";
+import { db } from "@/lib/db";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponseServerIo) {
+  if(req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
+  
+  try {
+    const profile = await currentProfilePages(req);
+    if(!profile) return res.status(401).json({ error: "Unauthorized" });
+    
+    const { serverId, channelId } = req.query;
+    if(!serverId) return res.status(400).json({ error: "Server ID missing" });
+    if(!channelId) return res.status(400).json({ error: "Channel ID missing" });
+    
+    const { content, fileUrl } = req.body;
+    if(!content) return res.status(400).json({ error: "Content missing" });
+    
+    const server = await db.server.findFirst({
+      where: {
+        id: serverId as string,
+        members: {
+          some: { profileId: profile.id },
+        },
+      },
+      include: { members: true }
+    });
+    if(!server) return res.status(404).json({ message: "Server not found" });
+    
+    const channel = await db.channel.findFirst({
+      where: {
+        id: channelId as string,
+        serverId: serverId as string,
+      },
+    });
+    if(!channel) return res.status(404).json({ message: "Channel not found" });
+    
+    const member = server.members.find(m => m.profileId === profile.id);
+    if(!member) return res.status(404).json({ message: "Member not found" });
+    
+    const message = await db.message.create({
+      data: { content, fileUrl, channelId: channel.id, memberId: member.id },
+      include: {
+        member: {
+          include: { profile: true },
+        },
+      },
+    });
+    
+    // emit message to all subscribed user through socket.io 
+    const channelKey = `chat:${channelId}:messages`;
+    res?.socket?.server?.io?.emit(channelKey, message);
+    
+    return res.status(200).json(message);
+  } catch (error) {
+    console.log("[MESSAGES_POST]", error);
+    return res.status(500).json({ error: "Internal Error" });
+  }
+}
