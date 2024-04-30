5c38c95bf858d76b8639aca23a26d3e183e8820d
Maulana Adam Sahid <masadamsahid@gmail.com>
2024-04-30 22:00:32 +0700

    fix using useParams

--- src/_components/navigation/navigation-item.tsx
+++ src/_components/navigation/navigation-item.tsx
@@ -30,14 +30,14 @@ const NavigationItem = ({ id, imageUrl, name, ...props }: Props) => {
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all size-[4px]",
-            params.serverId !== id && "group-hover:h-[20px]",
-            params.serverId === id ? "h-[36px]" : "h-[8px]"
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[36px]" : "h-[8px]"
           )}
         />
         <div
           className={cn(
             "relative group flex mx-3 size-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-            params.serverId === id && "bg-primary/10 text-primary rounded-[16px]"
+            params?.serverId === id && "bg-primary/10 text-primary rounded-[16px]"
           )}
         >
           <Image fill src={imageUrl} alt="Server"/>
